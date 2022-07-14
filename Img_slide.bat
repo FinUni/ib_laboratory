4d23e6e9e7fe7e2dd632472a46ae08cc891241c2
seregalsd <markin-2002@yandex.ru>
2022-07-14 19:22:22 +0300

    Issues resolved.

--- Img_slide.bat
+++ Img_slide.bat
@@ -1,16 +1,6 @@
-{\rtf1\ansi\ansicpg1251\cocoartf2638
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fmodern\fcharset0 Courier;}
-{\colortbl;\red255\green255\blue255;\red0\green0\blue0;\red255\green255\blue255;}
-{\*\expandedcolortbl;;\cssrgb\c0\c0\c0;\cssrgb\c100000\c100000\c99985;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
-\deftab720
-\pard\pardeftab720\partightenfactor0
-
-\f0\fs26 \cf2 \expnd0\expndtw0\kerning0
-\outl0\strokewidth0 \strokec3 :loop\
-timeout /t 1\
-@echo off\
-for %%i in (*.jpg) do start "" "%%i"&&>nul timeout /t 10\
-taskkill /F /IM Microsoft.Photos.exe\
-goto loop\
-}
+:loop
+timeout /t 1
+@echo off
+for %%i in (*.jpg) do start "" "%%i"&&>nul timeout /t 10
+taskkill /F /IM Microsoft.Photos.exe
+goto loop
