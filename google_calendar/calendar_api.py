c314cfb4ca17cc86dfb47e91e835109672fdaf38
seregalsd <markin-2002@yandex.ru>
2022-07-21 14:25:00 +0300

    Issues resolved.

--- google_calendar/calendar_api.py
+++ google_calendar/calendar_api.py
@@ -35,7 +35,7 @@ def GoogleCalendar():
             # Файл client_secret берётся в консоле гугла в разделе Credentials
             # Создаётся локальный сервер для авторизации пользователя
             # Вся необходимая для дальней авторизации записывается в файл token.json
-            flow = InstalledAppFlow.from_client_secrets_file(
+            flow = InstalledAppFlow.from_client_secrets_file( # todo: подтягивать данные онлайн
                 '/Users/sergeymarkin/Desktop/ib_laboratory/google_calendar/client_secret_122582591655-ommn1ml19bqlepf6vec55vujutsgn4ht.apps.googleusercontent.com.json', SCOPES)
             creds = flow.run_local_server(port=0, authorization_prompt_message='Для авторизации перейдите по ссылке:\n{url}',
                                           success_message='Авторизация прошла успешно!\nМожете закрыть эту страницу.',
@@ -51,25 +51,31 @@ def GoogleCalendar():
         # Call the Calendar API
         now = datetime.datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
         # Записываем в calendar_list названия созданных календарей и выводим данные из него
-        calendar_list = []
+        calendar_list = {}
         for calendar_list_entry in service.calendarList().list().execute()['items']:
-            calendar_list.append(calendar_list_entry['summary'])
-        events_result = service.events().list(calendarId=calendar_list[-1], timeMin=now,
-                                              singleEvents=True,
-                                              orderBy='startTime').execute()
-        events = events_result.get('items', [])
+            calendar_list[calendar_list_entry['id']] = calendar_list_entry['summary']
+        for num_calendar, num in zip(calendar_list, range(len(calendar_list))):
+            print(f'{num+1}. {calendar_list[num_calendar]}')
+        nums_calendar = str(input('Укажите номер календарей через пробел:\n')).split()
+        for num_calendar in nums_calendar:
+            events_result = service.events().list(calendarId=list(calendar_list)[int(num_calendar)-1], timeMin=now,
+                                                  singleEvents=True,
+                                                  orderBy='startTime').execute()
+            events = events_result.get('items', [])
 
-        if not events:
-            print('Нет предстоящих мероприятий.')
-            return
+            if not events:
+                print('Нет предстоящих мероприятий.')
+                return
 
-        # Выводим информацию о событиях
-        for event in events:
-            start = event['start'].get('dateTime', event['start'].get('date'))
-            if isinstance(['description'], str):
-                print(start, event['summary'], event['description'])
-            else:
-                print(start, event['summary'])
+            # Выводим информацию о событиях
+            for event in events:
+                start = event['start'].get('dateTime', event['start'].get('date'))
+                if isinstance(['description'], str):
+                    print(start, event['summary'], event['description'])
+                else:
+                    print(start, event['summary'])
 
     except HttpError as error:
         print('An error occurred: %s' % error)
+
+GoogleCalendar()
